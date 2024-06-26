ebe32828aabded50f5a8d647bd56239d0146035c
Daniel Ursul <ursuldm@gmail.com>
2024-06-27 00:34:54 +0300

    trying to set up docker

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -12,23 +12,22 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/ursuldaniel/bank-api/internal/server"
 	"github.com/ursuldaniel/bank-api/internal/storage"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	listenAddr := os.Getenv("listenAddr")
+	listenAddr := os.Getenv("LISTEN_ADDR")
 	if listenAddr == "" {
 		log.Fatal("missed server address")
 	}
 
-	storage, err := storage.NewPostgresStorage(context.TODO(), os.Getenv("connStr"))
+	storage, err := storage.NewPostgresStorage(context.TODO(), os.Getenv("CONN_STR"))
 	if err != nil {
 		log.Fatal(err)
 	}
